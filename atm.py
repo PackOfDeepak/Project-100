c4d87991122274f884945f72d32d8d72f787c159
PackOfDeepak <68823003+PackOfDeepak@users.noreply.github.com>
2021-12-05 08:06:26 -0600

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,11 @@
+class Atm(object):
+    def _init_(self, cardnumber, pin):
+        self.cardnumber = cardnumber
+        self.pin = pin
+    def Cashwithdrawal():
+        print("You have withdrawn")
+    def Deposit():
+        print("Your deposit is complete")
+    def BalanceEnquiry():
+        print("You have a bunch of money")
+JohnDoe = Atm(123456789,2345)
